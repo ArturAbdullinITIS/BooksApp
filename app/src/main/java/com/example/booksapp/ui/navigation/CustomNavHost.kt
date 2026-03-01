0b4bd71916747d7c73508eb328f63bf031156820
artur <arturaubdullin@gmail.com>
2026-03-02 02:57:40 +0300

    feat: implement basic bottom nav bar

--- app/src/main/java/com/example/booksapp/ui/navigation/CustomNavHost.kt
+++ app/src/main/java/com/example/booksapp/ui/navigation/CustomNavHost.kt
@@ -1,5 +1,9 @@
 package com.example.booksapp.ui.navigation
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -7,8 +11,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.example.booksapp.ui.screen.details.DetailsScreen
+import com.example.booksapp.ui.screen.favourite.FavouriteScreen
 import com.example.booksapp.ui.screen.main.MainScreen
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CustomNavHost(
     modifier: Modifier = Modifier
@@ -17,30 +23,51 @@ fun CustomNavHost(
         mutableStateListOf<Route>(Main)
     }
 
-    NavDisplay(
-        modifier = modifier,
-        backStack = backStack,
-        onBack = {
-            if(backStack.size > 1) {
-                backStack.removeLastOrNull()
-            }
-        },
-        entryProvider = entryProvider {
-            entry<Main> {
-                MainScreen(
-                    onNavigateToDetails = { bookId ->
-                        backStack.add(Details(bookId))
+    val currentRoute = backStack.lastOrNull() ?: Main
+
+    val isBottomBarVisible = currentRoute is Main || currentRoute is Favourites
+    Scaffold(
+        bottomBar = {
+            if(isBottomBarVisible) {
+                BottomNavBar(
+                    currentRoute = currentRoute,
+                    onSelected = { selectedRoute ->
+                        backStack.clear()
+                        backStack.add(selectedRoute)
                     }
                 )
             }
-            entry<Details> { route ->
-                DetailsScreen(
-                    bookId = route.bookId,
-                    onBack = {
-                        backStack.removeLastOrNull()
-                    },
-                )
-            }
         }
-    )
+    ) { innerPadding ->
+        NavDisplay(
+            modifier = modifier.padding(innerPadding),
+            backStack = backStack,
+            onBack = {
+                if(backStack.size > 1) {
+                    backStack.removeLastOrNull()
+                }
+            },
+            entryProvider = entryProvider {
+                entry<Main> {
+                    MainScreen(
+                        onNavigateToDetails = { bookId ->
+                            backStack.add(Details(bookId))
+                        }
+                    )
+                }
+                entry<Details> { route ->
+                    DetailsScreen(
+                        bookId = route.bookId,
+                        onBack = {
+                            backStack.removeLastOrNull()
+                        },
+                    )
+                }
+                entry<Favourites> { route ->
+                    FavouriteScreen()
+                }
+            }
+        )
+    }
+
 }
---
+++ app/src/main/java/com/example/booksapp/ui/screen/favourite/FavouriteScreen.kt
@@ -0,0 +1,12 @@
+package com.example.booksapp.ui.screen.favourite
+
+import androidx.compose.runtime.Composable
+import com.example.booksapp.ui.navigation.Favourites
+
+
+@Composable
+fun FavouriteScreen(
+
+) {
+
+}
